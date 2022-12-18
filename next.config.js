2e4ee4d554c048233f5c66b3ccd80cfb751c54a9
Arianna <arianna.stradolini@gmail.com>
2022-12-18 15:37:25 +0100

    images

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const { i18n } = require("./next-i18next.config");
 module.exports = {
   reactStrictMode: false,
   i18n,
+  images: {
+    domains: ['static.mydomain.com'],
+    disableStaticImages: true
+},
   
   
 }
